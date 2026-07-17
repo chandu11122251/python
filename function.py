b0cc98a00fc8089a84df3fa085f5a843180d7871
chandu11122251 <mavoorichandrashekhar@gmail.com>
2026-07-17 23:34:54 +0530

    added functions

---
+++ function.py
@@ -0,0 +1,7 @@
+name = input("enter your name: ")
+college = input("enter college name: ")
+branch = input("enter your branch: ")
+def student():
+    print(f"NAME : {name}\nCOLLEGE : {college}\nBRANCH : {branch}")
+student()    
+    
